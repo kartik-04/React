6003500cd514efbb31fdf65224c16571d7975777
kartik-04 <04kartik04@gmail.com>
2025-06-01 14:35:13 +0530

    implementing pagination

--- Class4/imdb-app/src/components/Pagination.jsx
+++ Class4/imdb-app/src/components/Pagination.jsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const Pagination = ({ onPageChange }) => {
-  const [Pages, setCurrentPage] = useState([2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  const [selectedPage, setSelectedPage] = useState(Pages[0]);
+const Pagination = ({ onPageChange, totalPages }) => {
+  const [Pages, setPages] = useState([]);
+  const [selectedPage, setSelectedPage] = useState(1);
+  const [pageLimit, setPageLimit] = useState();
+  const threshold = 10;
+
+  const setNewPageLimit = (page) => {
+    let half = Math.floor(threshold / 2);
+    let start = Math.max(1, page - half);
+    let end = Math.min(totalPages, start + threshold - 1);
+
+    if (end - start + 1 < threshold) {
+      start = Math.max(1, end - threshold + 1);
+    }
+    const list = Array.from({ length: end - start + 1 }, (_, i) => i + start);
+    setPages(list);
+  };
 
   const handleChange = (page) => {
     onPageChange(page);
     setSelectedPage(page);
+    setNewPageLimit(page);
   };
+
+  useEffect(() => {
+    setPageLimit(totalPages);
+    const list = Array.from(
+      { length: Math.min(totalPages, threshold) },
+      (_, i) => i + 1
+    );
+    setPages(list);
+  }, [totalPages]);
+
   return (
     <div className="pagination">
       <button
